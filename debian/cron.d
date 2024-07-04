c581134f19bcf70cfcb3126479ceaf15f7cb9b8f
Camille Lafitte <alternc@webelys.com>
2024-07-04 11:09:40 +0200

    Provide a standard packaging files
    * Set default packaging content to allow a build
    * As codebase is old we consider an old AlternC compatibility

---
+++ debian/cron.d
@@ -0,0 +1,6 @@
+# /etc/cron.d/spip_ecran_secu: crontab entries for the alternc-spip package
+#
+SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+* * * * * root cd /usr/lib/alternc && php httpperm_cron.php &>/dev/null
